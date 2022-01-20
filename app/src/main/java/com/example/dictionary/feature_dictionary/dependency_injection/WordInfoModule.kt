0626bddfcb42c084857125df38f2c3d24eeeee2c
Shane Rex <shanerex2001@gmail.com>
2022-01-20 13:15:13 +0530

    Finished setting up DaggerHilt

---
+++ app/src/main/java/com/example/dictionary/feature_dictionary/dependency_injection/WordInfoModule.kt
@@ -0,0 +1,63 @@
+package com.example.dictionary.feature_dictionary.dependency_injection
+
+import android.app.Application
+import androidx.room.Room
+import com.example.dictionary.feature_dictionary.data.local.WordInfoDatabase
+import com.example.dictionary.feature_dictionary.data.remote.DictionaryAPI
+import com.example.dictionary.feature_dictionary.data.repository.WordInfoRepositoryImplementation
+import com.example.dictionary.feature_dictionary.data.util.GsonParser
+import com.example.dictionary.feature_dictionary.domain.repository.WordInfoRepository
+import com.example.dictionary.feature_dictionary.domain.use_case.GetWordInfo
+import com.google.gson.Gson
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object WordInfoModule {
+
+    @Provides
+    @Singleton
+    fun providesGetWordInfoUseCase(repository: WordInfoRepository): GetWordInfo {
+        return GetWordInfo(repository)
+    }
+
+    @Provides
+    @Singleton
+    fun providesWordInfoRepository(
+        api: DictionaryAPI,
+        db: WordInfoDatabase
+    ): WordInfoRepository {
+        return WordInfoRepositoryImplementation(
+            api,
+            db.dao
+        )
+    }
+
+    @Provides
+    @Singleton
+    fun providesWordInfoDatabase(app: Application): WordInfoDatabase {
+        return Room.databaseBuilder(
+            app,
+            WordInfoDatabase::class.java,
+            "word_info_db"
+        ).addTypeConverter(GsonParser(Gson()))
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun providesDictionaryAPI(): DictionaryAPI {
+        return Retrofit.Builder()
+            .baseUrl(DictionaryAPI.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(DictionaryAPI::class.java)
+    }
+}
